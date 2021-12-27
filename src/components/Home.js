7e96d3a143564efe90d06d2ae4e3401af4992142
cspdevs000 <cspdevs000@gmail.com>
2021-12-27 17:13:59 -0500

    router setup, home page added

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <h1>home page yee</h1>
+        );
+    }
+}
+
+export default Home;
